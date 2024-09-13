890ec7376a02424ef0f6d7067307dbb1915d43dc
Victoria Argañaras <varganaras@cuentamono.com>
2024-09-13 03:43:50 -0300

    redirect to guess when logged in at "/"

--- pento/lib/pento_web/controllers/page_controller.ex
+++ pento/lib/pento_web/controllers/page_controller.ex
@@ -1,9 +1,14 @@
 defmodule PentoWeb.PageController do
   use PentoWeb, :controller
+  alias Phoenix.Controller
 
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    if conn.assigns.current_user !== nil do
+      Controller.redirect(conn, to: ~p"/guess")
+    else
+      render(conn, :home, layout: false)
+    end
   end
 end
