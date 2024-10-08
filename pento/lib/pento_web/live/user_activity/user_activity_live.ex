92d074d4a6cdad3bb5af7f32e3f07c35cb4008c8
Victoria Argañaras <varganaras@cuentamono.com>
2024-10-08 19:20:21 -0300

    add user activity

---
+++ pento/lib/pento_web/live/user_activity/user_activity_live.ex
@@ -0,0 +1,35 @@
+defmodule PentoWeb.Admin.UserActivityLive do
+  use PentoWeb, :live_component
+  alias PentoWeb.Presence
+
+  @user_activity_topic "user_activity"
+
+  def update(assigns, socket) do
+    {:ok,
+     socket
+     |> assign_user_activity()}
+  end
+
+  def assign_user_activity(socket) do
+    assign(socket, :user_activity, list_products_and_users())
+  end
+
+  def list_products_and_users() do
+    Presence.list(@user_activity_topic)
+    |> Enum.map(&extract_product_with_users/1)
+  end
+
+  defp extract_product_with_users({product_name, %{metas: metas}}) do
+    {product_name, users_from_metas_list(metas)}
+  end
+
+  defp users_from_metas_list(metas_list) do
+    Enum.map(metas_list, &users_from_meta_map/1)
+    |> List.flatten()
+    |> Enum.uniq()
+  end
+
+  def users_from_meta_map(meta_map) do
+    get_in(meta_map, [:users])
+  end
+end
